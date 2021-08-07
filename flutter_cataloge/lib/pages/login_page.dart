27cbe7242d8264442be10f42f9f79dfd12e31f54
SWALI SAXENA <swalisaxena6@gmail.com>
2021-08-07 10:41:57 +0530

    Day4 completed

--- flutter_cataloge/lib/pages/login_page.dart
+++ flutter_cataloge/lib/pages/login_page.dart
@@ -5,19 +5,47 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext cntext) {
     return Material(
       color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset("assets/Login_page.png",
-            fit: BoxFit.fitHeight,
-            height: 250,
-            
-            ),
-            Text("Welcome",
-            style: TextStyle(fontSize: 22,
-            fontWeight:FontWeight.bold),
-            )
-          ],
-          )
+      child: Column(children: [
+        Image.asset(
+          "assets/Login_page.png",
+          fit: BoxFit.fitHeight,
+          height: 250,
+        ),
+        SizedBox(height: 20.0),
+        Text(
+          "Welcome",
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Enter Username", labelText: "Username"),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Enter Password", labelText: "Password"),
+              ),
+              ElevatedButton(
+                child: Text("Login"),
+                style: TextButton.styleFrom(),
+                onPressed: () {
+                  print("Hello Swali");
+                },
+              )
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
